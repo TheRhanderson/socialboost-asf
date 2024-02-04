7e2f875ad993a3634d3a13577282afb0ff1623f4
Rhanderson M.F <rhandinho10@gmail.com>
2024-02-04 03:52:05 -0300

    CSHAREDLIKE

---
+++ SocialBoost/CSharedLike.cs
@@ -0,0 +1,142 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using ArchiSteamFarm.Core;
+using ArchiSteamFarm.Steam.Interaction;
+using ArchiSteamFarm.Steam;
+using ArchiSteamFarm.Localization;
+using ArchiSteamFarm.Web.Responses;
+using SocialBoost.Helpers;
+using static ArchiSteamFarm.Steam.Integration.ArchiWebHandler;
+
+namespace SocialBoost;
+internal static class CSharedLike {
+
+	public static int NumEnviosDesejados;
+
+	public static async Task<string?> EnviarLikeSharedfiles(Bot bot, EAccess access, string id) {
+
+		if (access < EAccess.Master) {
+			return null;
+		}
+
+		bool? botUtilizadoAnteriormente = await DbHelper.VerificarEnvioItem(bot.BotName, "SHAREDLIKE", id).ConfigureAwait(false);
+
+		if (botUtilizadoAnteriormente == true) {
+			return null;
+		}
+
+		if (!bot.IsConnectedAndLoggedOn) {
+			return null;
+		}
+
+		if (bot.IsAccountLimited) {
+			return null;
+		}
+
+		//string? sessionId = await FetchSessionID(bot).ConfigureAwait(false);
+		bot.ArchiLogger.LogGenericInfo($"SocialBoost|SHAREDFILES|LIKE => {id} (Enviando)");
+
+		Uri request = new(SteamCommunityURL, "/sharedfiles/voteup");
+		Uri requestViewPage = new(SteamCommunityURL, $"/sharedfiles/filedetails/?id={id}");
+
+		Dictionary<string, string> data = new(1)
+		{
+		{ "id", id }
+		};
+
+		bool? verificaSharedFav = await DbHelper.VerificarEnvioItem(bot.BotName, "SHAREDFAV", id).ConfigureAwait(false);
+		// verificamos se esse bot já enviou favoritos antes, pois então não precisamos 'visitar' a página novamente.
+		if (verificaSharedFav == false) {
+			HtmlDocumentResponse? response = await VisualizarPagina(bot, requestViewPage).ConfigureAwait(false);
+			if (response == null) {
+				bot.ArchiLogger.LogGenericError("Erro ao executar POST");
+			}
+		}
+
+		bool postSuccess = await bot.ArchiWebHandler.UrlPostWithSession(request, data: data, session: ESession.Lowercase, referer: requestViewPage).ConfigureAwait(false);
+
+		if (!postSuccess) {
+			bot.ArchiLogger.LogGenericError("Erro ao executar POST");
+		}
+
+		bool? salvaItem = await DbHelper.AdicionarEnvioItem(bot.BotName, "SHAREDLIKE", id).ConfigureAwait(false);
+
+		if (!salvaItem.HasValue) {
+			return null;
+		}
+
+		bot.ArchiLogger.LogGenericInfo($"SocialBoost|SHAREDFILES|LIKE => {id} (OK)");
+		return bot.Commands.FormatBotResponse(postSuccess ? $"{Strings.Success.Trim()} — ID: {id} — Like" : Strings.WarningFailed);
+
+	}
+
+	internal static async Task<HtmlDocumentResponse?> VisualizarPagina(Bot bot, Uri requestViewPage) {
+		HtmlDocumentResponse? response = await bot.ArchiWebHandler.UrlGetToHtmlDocumentWithSession(requestViewPage, referer: SteamCommunityURL).ConfigureAwait(false);
+		return response;
+	}
+
+	public static async Task<string?> EnviarLikeSharedfiles(EAccess access, ulong steamID, string argument, string envioDesejado) {
+
+		if (string.IsNullOrEmpty(argument) || string.IsNullOrEmpty(envioDesejado)) {
+			ASF.ArchiLogger.LogNullError(null, nameof(argument) + " || " + nameof(envioDesejado));
+
+			return null;
+		}
+
+		if (int.TryParse(envioDesejado, out int numEnviosDesejados)) {
+			NumEnviosDesejados = numEnviosDesejados;
+		} else {
+			return null;
+		}
+
+		HashSet<Bot>? bots = Bot.GetBots("ASF");
+
+		if ((bots == null) || (bots.Count == 0)) {
+			return access >= EAccess.Owner ? FormatBotResponse(Strings.BotNotFound, "SocialBoost") : null;
+		}
+
+		int contador = 0;
+
+		foreach (Bot bot in bots) {
+			if (bot.IsConnectedAndLoggedOn) {
+				contador++;
+
+				if (contador == numEnviosDesejados) {
+					break;
+				}
+			}
+		}
+
+		if (contador != numEnviosDesejados) {
+			return access >= EAccess.Owner ? FormatBotResponse("Não há bots online suficientes!", "SocialBoost") : null;
+		}
+
+		bool? logger = await DSKLogger.CompartilharAtividade($"CSharedfiles-LIKE-{argument}").ConfigureAwait(false);
+
+		List<string?> responses = []; // Lista para armazenar respostas bem-sucedidas
+		int enviosBemSucedidos = 0; // Contador de envios bem-sucedidos
+
+		foreach (Bot bot in bots) {
+			if (enviosBemSucedidos >= NumEnviosDesejados) {
+				break; // Saia do loop se o número desejado já foi atingido
+			}
+
+			string? response = await EnviarLikeSharedfiles(bot, Commands.GetProxyAccess(bot, access, steamID), argument).ConfigureAwait(false);
+
+			if (!string.IsNullOrEmpty(response)) {
+				responses.Add(response);
+				enviosBemSucedidos++;
+			}
+		}
+
+		ASF.ArchiLogger.LogGenericInfo($"Envio concluído! Criado por @therhanderson");
+		return responses.Count > 0 ? $"{string.Join(Environment.NewLine, responses)}{Environment.NewLine}Enviados com sucesso: {enviosBemSucedidos}" : null;
+	}
+
+	public static async Task<string?> FetchSessionID(Bot bot) =>
+	await SessionHelper.FetchSessionID(bot).ConfigureAwait(false);
+
+	private static string FormatBotResponse(string message, string botName) =>
+		$"<{botName}> {message}";
+}
--- SocialBoost/Helpers/SessionHelper.cs
+++ SocialBoost/Helpers/SessionHelper.cs
@@ -2,16 +2,16 @@ using System;
 using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
-using ArchiSteamFarm.Steam.Integration;
 using ArchiSteamFarm.Steam;
 using ArchiSteamFarm.Core;
 using ArchiSteamFarm.Web.Responses;
 using System.Text.RegularExpressions;
+using static ArchiSteamFarm.Steam.Integration.ArchiWebHandler;
 
 namespace SocialBoost.Helpers;
 internal static class SessionHelper {
 	public static async Task<string?> FetchSessionID(Bot bot) {
-		CookieCollection cc = bot.ArchiWebHandler.WebBrowser.CookieContainer.GetCookies(ArchiWebHandler.SteamStoreURL);
+		CookieCollection cc = bot.ArchiWebHandler.WebBrowser.CookieContainer.GetCookies(SteamStoreURL);
 		Cookie? sessionIdCookie = cc.Cast<Cookie>().FirstOrDefault(c => c.Name.Equals("sessionid", StringComparison.OrdinalIgnoreCase));
 
 		return sessionIdCookie != null
@@ -22,7 +22,7 @@ internal static class SessionHelper {
 	internal static async Task<string?> FetchReviewID(string urlReview) {
 
 		Uri uri2 = new(urlReview);
-		HtmlDocumentResponse? response = await ASF.WebBrowser!.UrlGetToHtmlDocument(uri2, referer: ArchiWebHandler.SteamCommunityURL).ConfigureAwait(false);
+		HtmlDocumentResponse? response = await ASF.WebBrowser!.UrlGetToHtmlDocument(uri2, referer: SteamCommunityURL).ConfigureAwait(false);
 
 		if (response == null || response.Content?.Body == null) {
 			ASF.ArchiLogger.LogGenericError("A requisição não retornou uma resposta válida.");
@@ -47,7 +47,7 @@ internal static class SessionHelper {
 
 	internal static async Task<string?> FetchAppIDShared(string urlReview) {
 		Uri uri2 = new(urlReview);
-		HtmlDocumentResponse? response = await ASF.WebBrowser!.UrlGetToHtmlDocument(uri2, referer: ArchiWebHandler.SteamCommunityURL).ConfigureAwait(false);
+		HtmlDocumentResponse? response = await ASF.WebBrowser!.UrlGetToHtmlDocument(uri2, referer: SteamCommunityURL).ConfigureAwait(false);
 
 		if (response == null || response.Content?.Body == null) {
 			ASF.ArchiLogger.LogGenericError("A requisição não retornou uma resposta válida.");
@@ -70,7 +70,7 @@ internal static class SessionHelper {
 
 	internal static async Task<string?> FetchSteamID64(string urlReview) {
 		Uri uri2 = new(urlReview);
-		HtmlDocumentResponse? response = await ASF.WebBrowser!.UrlGetToHtmlDocument(uri2, referer: ArchiWebHandler.SteamCommunityURL).ConfigureAwait(false);
+		HtmlDocumentResponse? response = await ASF.WebBrowser!.UrlGetToHtmlDocument(uri2, referer: SteamCommunityURL).ConfigureAwait(false);
 
 		if (response == null || response.Content?.Body == null) {
 			ASF.ArchiLogger.LogGenericError("A requisição não retornou uma resposta válida.");
